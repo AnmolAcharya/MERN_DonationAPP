799d767df4030ee8101518d976dfabf803d13197
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-01 01:42:25 -0600

    deploying services

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/messages/send",
+        "https://mern-donationapp-1.onrender.com/api/v1/messages/send",
         { name, email, phone, message },
         {
           withCredentials: true,
